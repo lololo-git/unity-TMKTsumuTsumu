150f3144693840301982ab086512bfba55b09248
Soshi H <soshi@lolol.ooo>
2022-08-11 16:37:11 +0900

    add generator and prefabs

---
+++ Assets/Scripts/BallGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallGenerator : MonoBehaviour
+{
+    [SerializeField] GameObject ballPrefab = default;
+
+    private void Start()
+    {
+        StartCoroutine(Spawns(40));
+    }
+
+
+    public IEnumerator Spawns(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Vector2 pos = new Vector2(Random.Range(-0.2f, 0.2f), 8f);
+            Instantiate(ballPrefab, pos, Quaternion.identity);
+            yield return new WaitForSeconds(0.04f);
+        }
+    }
+
+
+}
