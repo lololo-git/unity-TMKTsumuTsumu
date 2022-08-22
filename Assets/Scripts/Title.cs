e3bf2527bdf090b8c46ea4c765d844d278b08ef1
Soshi H <soshi@lolol.ooo>
2022-08-22 10:53:41 +0900

    add Title Scene

---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    //[SerializeField]
+
+    public void OnStartButton()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
